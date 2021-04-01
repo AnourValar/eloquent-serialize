ae8dbbb84af2f74356f4d33c1cace7b3111b3bfd
Fatih Aytekin <fatih@aytekin.me>
2021-04-01 14:40:46 +0300

    Add facades PHPDocs

--- src/Facades/EloquentSerializeFacade.php
+++ src/Facades/EloquentSerializeFacade.php
@@ -4,6 +4,10 @@ namespace AnourValar\EloquentSerialize\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \AnourValar\EloquentSerialize\Service serialize(\Illuminate\Database\Eloquent\Builder $builder)
+ * @method static \AnourValar\EloquentSerialize\Service unserialize($package)
+ */
 class EloquentSerializeFacade extends Facade
 {
     /**
