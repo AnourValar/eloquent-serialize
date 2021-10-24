18269ce0e46fe80aed1ed8c4fbc179dd7747e856
ivalkenburg <i.valkenburg@ziggo.nl>
2021-10-24 17:31:28 +0200

    Fix facade docblocks

--- src/Facades/EloquentSerializeFacade.php
+++ src/Facades/EloquentSerializeFacade.php
@@ -5,8 +5,8 @@ namespace AnourValar\EloquentSerialize\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static \AnourValar\EloquentSerialize\Service serialize(\Illuminate\Database\Eloquent\Builder $builder)
- * @method static \AnourValar\EloquentSerialize\Service unserialize($package)
+ * @method static string serialize(\Illuminate\Database\Eloquent\Builder $builder)
+ * @method static \Illuminate\Database\Eloquent\Builder unserialize(mixed $package)
  */
 class EloquentSerializeFacade extends Facade
 {
