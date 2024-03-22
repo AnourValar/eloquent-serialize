6e91093c10940859c4b0549b6a90f18d8db45998
Nikolay Novikov <AnourValar@gmail.com>
2024-03-22 15:56:46 +0300

    tests updates

--- tests/LimitTest.php
+++ tests/LimitTest.php
@@ -17,4 +17,13 @@ class LimitTest extends AbstractSuite
         // Limit with offset
         $this->compare(User::offset(20)->limit(10));
     }
+
+    /**
+     * @return void
+     */
+    public function testNested()
+    {
+        $this->compare(User::with(['userPhones' => fn ($query) => $query->limit(1)]));
+        $this->compare(User::with(['userPhones' => fn ($query) => $query->limit(2)]));
+    }
 }
